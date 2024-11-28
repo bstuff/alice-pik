14ae587cb78e2c81386d75b168370a00d0b901eb
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2024-11-28 23:50:29 +0300

    step 1

--- app/routes/login.ya._index.tsx
+++ app/routes/login.ya._index.tsx
@@ -8,6 +8,7 @@ import { routes } from '~/utils/routes';
 
 export const loader = (async ({ request }) => {
   const params = getQueryParams<{ code?: string; redirect?: string }>(request.url);
+  console.log('>>>', params, request.url)
   return json({
     origin: new URL(request.url).origin.replace(/^http:/, 'https:'),
     redirect: params.redirect,
@@ -33,6 +34,14 @@ export default function LoginYaPage() {
     <ContentContainer>
       <h1 className="pt-10 text-center text-2xl">Login</h1>
 
+      <div className="mx-auto mt-8 w-full max-w-[600px]">
+        <ul className="steps steps-vertical">
+          <li className="step step-primary">Шаг 1: Войти при помощи Яндекс-аккаунта</li>
+          <li className="step">Шаг 2: Получение токена для управления устройствами</li>
+          <li className="step">Шаг 3: Выбор устройств, доступных в умном доме</li>
+          <li className="step">Шаг 4: Привязка аккаунта к умному дому</li>
+        </ul>
+      </div>
       <div className="mx-auto mt-8 w-full max-w-[600px]">
         <Link className="btn btn-accent" to={yaLink}>
           Войти
