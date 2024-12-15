64710414678392621d6dcf8c0cd47fed03df0fef
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2024-12-15 21:56:33 +0300

    Capture distinct uid

--- app/routes/alice.[v1.0].user.devices.action.ts
+++ app/routes/alice.[v1.0].user.devices.action.ts
@@ -21,7 +21,7 @@ export const action = (async ({
   const pikToken = await getPikToken({ request, context });
   invariant(pikToken);
 
-  context.posthog.capture({ event: 'Devices/Action', distinctId: 'devices_action' });
+  context.posthog.capture({ event: 'Devices/Action', distinctId: `ya:${user.uid}` });
 
   const reqJson = (await request.json()) as StateChangeRequest;
   // >>> {"payload":{"devices":[{"id":"pik:relay:40625","capabilities":[{"type":"devices.capabilities.on_off","state":{"instance":"on","value":true}}],"custom_data":{"_provider":"pik","type":"relay"}}]}}
