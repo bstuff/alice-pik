c1d155b2f90ab5983acf820c352d5025aaf2b56b
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2024-12-16 23:59:45 +0300

    Devices/Unlink

--- app/routes/alice.[v1.0].user.unlink.ts
+++ app/routes/alice.[v1.0].user.unlink.ts
@@ -1,6 +1,13 @@
 import { ActionFunction, json } from '@remix-run/cloudflare';
+import { getUser } from '~/utils/auth';
 
-export const action: ActionFunction = ({context}) => {
-  context.posthog.capture({ event: 'Devices/Unlink', distinctId: 'devices_unlink' });
+export const action: ActionFunction = async ({ request, context }) => {
+  const user = await getUser({ request, context });
+  if (!user) {
+    throw new Response(null, { status: 401 });
+  }
+
+  context.posthog.capture({ event: 'Devices/Unlink', distinctId: `ya:${user.uid}` });
+
   return json(null);
 };
